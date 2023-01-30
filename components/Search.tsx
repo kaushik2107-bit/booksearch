24b3551a5f109a206d28ee61e4f7790b3839bc06
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2023-01-30 21:35:50 +0530

    fixed mixed content blocking

--- components/Search.tsx
+++ components/Search.tsx
@@ -29,6 +29,15 @@ function Search() {
     setData(data);
   };
 
+  const secure = (str: string) => {
+    if (str.startsWith("https://")) return str;
+
+    const ans = str.split("");
+    ans.splice(4, 0, "s");
+    const a = ans.join();
+    return a;
+  };
+
   return (
     <div className="flex justify-center bg-gray-600 py-4">
       <div className="grid lg:grid-cols-2 grid-cols-1 md:w-[80vw] lg:w-[70vw] w-[90vw] gap-2 grid-rows-4">
@@ -41,8 +50,8 @@ function Search() {
             >
               <div className="bg-gray-400 aspect-square h-full rounded-[5px] relative">
                 <Image
-                  loader={() => item.image}
-                  src={item.image}
+                  loader={() => secure(item.image)}
+                  src={secure(item.image)}
                   alt={"BOOK"}
                   fill
                   style={{ objectFit: "cover" }}
